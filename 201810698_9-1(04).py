a3102aa3373b7ee534c9945f0c39fdea08437beb
이병철 <lbc0729@naver.com>
2025-06-13 19:33:41 +0900

    9주차

---
+++ 201810698_9-1(04).py
@@ -0,0 +1,18 @@
+#self-study 10-1
+
+from tkinter import *
+
+window = Tk()
+window.title("냥이들^^")
+
+
+photo1 = PhotoImage(file="gif/cat1.gif")
+photo2 = PhotoImage(file="gif/cat2.gif") 
+
+label1 = Label(window, image=photo1)
+label2 = Label(window, image=photo2)
+
+label1.pack(side=LEFT)
+label2.pack(side=LEFT)    
+
+window.mainloop()
